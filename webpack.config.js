6e3bf891af17f8236bceb5dbac28fbc688fd228c
Dejan Duracoski <dedurac@gmail.com>
2017-10-19 17:24:34 +0200

    added UglifyJS

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+
 module.exports = {
   entry: './index.js',
   output: {
     filename: 'bundle.js'
   },
+  plugins: [
+      new UglifyJSPlugin()
+    ],
   module: {
     rules: [
       {
